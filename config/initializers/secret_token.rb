3999f3c7521da771b72584e54adfd16e43a72b90
solimantamu <solimantamu@gmail.com>
2022-11-11 00:15:28 +0100

    Delete config directory

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '133dd6c0f41dc3b9eff7d7fabed029aa055e32134d78e2fe5ce6c527f2ff5c0d6e423d09253037abea74bf0dcbe70c51f5c1022e3eb235bae2beab3626e6970e'
